ef7dbc55441ac100b149b3910893b34f08ddb972
Rafiul Islam <sohan@redlimesolutions.com>
2026-07-27 11:20:08 +0600

    make the layout responsive

--- src/Farm360.Web/src/app/core/layout/main-layout/main-layout.component.ts
+++ src/Farm360.Web/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { HeaderComponent } from '../header/header.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-layout',
@@ -17,11 +20,37 @@ import { HeaderComponent } from '../header/header.component';
   ],
   templateUrl: './main-layout.component.html'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit, OnDestroy {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   isSidebarCollapsed = false;
+  isMobile = false;
+  private destroy$ = new Subject<void>();
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(max-width: 768px)'])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+        if (this.isMobile) {
+          // On mobile, sidebar is not collapsed, it's just hidden (over mode)
+          this.isSidebarCollapsed = false;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   toggleSidebar() {
-    this.isSidebarCollapsed = !this.isSidebarCollapsed;
+    if (this.isMobile) {
+      this.sidenav.toggle();
+    } else {
+      this.isSidebarCollapsed = !this.isSidebarCollapsed;
+    }
   }
 }
